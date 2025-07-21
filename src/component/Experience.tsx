6bf72e6284f839109c872f430bfe197cd5b99cdc
Anthony Bautista <anthony@mccrev.com>
2025-07-22 02:40:05 +0800

    Add Experience component with tech stack and work details

---
+++ src/component/Experience.tsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import ShiftTabs from "./animata/container/shift-tabs";
+
+const techstack = [
+  "HTML",
+  "CSS",
+  "Tailwind",
+  "Bootstrap",
+  "Javascript",
+  "Typescript",
+  "ReactJs",
+  "ExpressJs",
+  "NodeJs",
+  "MongoDB",
+  "NextJs",
+  "Svelte",
+  "SQLite",
+  "Ruby",
+  "Ruby-on-Rails",
+  "PostgreSQL",
+  "Figma",
+  "Appwrite",
+  "Supabase",
+  "Vite",
+  "VScode",
+  "Jquery",
+  "Shadcn",
+  "Claude-AI",
+  "Chat-GPT",
+];
+
+const frontEnd = [
+  "HTML",
+  "CSS",
+  "Tailwind",
+  "Bootstrap",
+  "Javascript",
+  "Typescript",
+  "ReactJs",
+  "NextJs",
+  "Svelte",
+  "Jquery",
+  "Shadcn",
+  "Figma",
+];
+
+const backEnd = [
+  "ExpressJs",
+  "NodeJs",
+  "MongoDB",
+  "SQLite",
+  "Ruby",
+  "Ruby-on-Rails",
+  "PostgreSQL",
+  "Appwrite",
+  "Supabase",
+];
+
+const toolsAndLibraries = ["Vite", "VScode", "Claude-AI", "Chat-GPT"];
+
+const Experience = () => {
+  const [activeTab, setActiveTab] = useState<number | null>(0);
+
+  const renderTechStack = () => {
+    switch (activeTab) {
+      case 0:
+        return techstack;
+      case 1:
+        return frontEnd;
+      case 2:
+        return backEnd;
+      case 3:
+        return toolsAndLibraries;
+      default:
+        return techstack;
+    }
+  };
+
+  return (
+    <div className="py-10 bg-gradient-to-r from-[#10131a] to-[#0f212c]">
+      <div className="container mx-auto px-4">
+        <h1 className="text-[#EFF0F2] text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center">
+          Working <span className="text-[#2EB2D3]">Experience</span>  
+        </h1>
+        <div className="flex flex-col lg:flex-row gap-10">
+          <div className="text-[#EFF0F2] lg:w-1/2">
+            <h2 className="text-[#EFF0F2] text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">CBS OffShore</h2>
+            <div className="mb-8">
+              <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">March 2024 -  July 2025</p>
+              <p className="text-[#2EB2D3] font-bold text-base md:text-lg lg:text-xl leading-relaxed tracking-wide">
+                Remote
+              </p>
+              <p className="text-[#2EB2D3] font-bold text-base md:text-lg lg:text-xl leading-relaxed tracking-wide">
+                Front-End Developer
+              </p>
+              <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">
+                 I worked as a front-end developer building multiple direct-to-sale e-commerce websites using HTML, Bootstrap, jQuery, and .NET. I converted Figma prototypes into responsive, mobile-friendly websites and optimized performance with GTmetrix, achieving an A+ score, 90% performance, and 1s load time. I integrated tools like Google Tag Manager, Facebook Pixel, TikTok Pixel, and Everflow for conversion tracking. We also used CRMs like Konnektive and Sticky.io to manage customer data, subscriptions, and streamline sales operations.
+              </p>
+            </div>
+            <div>
+              <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">2018 - 2019</p>
+              <p className="text-[#2EB2D3] font-bold text-base md:text-lg lg:text-xl leading-relaxed tracking-wide">
+                Talisay City College
+              </p>
+              <p className="text-[#2EB2D3] font-bold text-base md:text-lg lg:text-xl leading-relaxed tracking-wide">
+                Bachelor of Science in Industrial Technology
+              </p>
+              <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni odio cupiditate ut deleniti aperiam et
+                quibusdam quidem totam. Mollitia, ullam minima quis quaerat eos recusandae veniam in odit aperiam
+                libero.
+              </p>
+            </div>
+          </div>
+          <div className="text-center lg:w-1/2">
+            <h2 className="text-[#EFF0F2] text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">Certificate</h2>
+            
+             
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
