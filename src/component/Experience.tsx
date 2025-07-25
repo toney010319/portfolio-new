92cb310f40aa30df4744c7e6074d0c36533194ef
Anthony Bautista <anthony@mccrev.com>
2025-07-26 04:11:39 +0800

    update

--- src/component/Experience.tsx
+++ src/component/Experience.tsx
@@ -7,8 +7,8 @@ const Experience = () => {
         <h1 className="text-[#EFF0F2] text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center">
           Professional <span className="text-[#2EB2D3]">Experience</span>
         </h1>
-        <div data-aos-delay="400" className="flex flex-col lg:flex-row gap-10">
-          <div data-aos="fade-up " className="text-[#EFF0F2] lg:w-1/2">
+        <div data-aos="fade-up" data-aos-delay="150" className="flex flex-col lg:flex-row gap-10">
+          <div className="text-[#EFF0F2] lg:w-1/2">
             <h2 className="text-[#EFF0F2] text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">CBS OffShore</h2>
             <div className="mb-8">
               <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">
