159a235ba78e0bfa03031b2b3b28d92efdf7a671
Anthony Bautista <anthony@mccrev.com>
2025-07-25 22:54:06 +0800

    add Footer component to enhance site layout and provide copyright information

---
+++ src/component/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <>
+      <div className='bg-gray-800 text-white text-center p-4'>
+        <p className='text-sm'>© 2023 Your Name. All rights reserved.</p>
+        <p className='text-xs'>Built with React and Tailwind CSS</p>
+      </div>
+      </>
+  )
+}
+
+export default Footer
