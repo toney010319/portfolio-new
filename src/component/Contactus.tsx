c0dff09eb09f7f268333772f39e49139b3f922ba
Anthony Bautista <anthony@mccrev.com>
2025-07-25 23:01:28 +0800

    build fix error

--- src/component/Contactus.tsx
+++ src/component/Contactus.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+ 
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Input } from '@/components/ui/input';
