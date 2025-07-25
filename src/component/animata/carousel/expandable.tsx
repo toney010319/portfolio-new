81d03b8fbb7b026815d8615d5088caba8bd8992a
Anthony Bautista <anthony@mccrev.com>
2025-07-25 22:54:18 +0800

    add Expandable component for image carousel with autoplay functionality

---
+++ src/component/animata/carousel/expandable.tsx
@@ -0,0 +1,104 @@
+import { HTMLAttributes, useEffect, useState } from "react";
+
+ 
+import { cn } from "@/lib/utils";
+import WaveReveal from "../text/wave-reveal";
+
+interface ImageProps extends HTMLAttributes<HTMLDivElement> {
+  item: { image: string; title: string };
+  index: number;
+  activeItem: number;
+}
+
+interface ExpandableProps {
+  list?: { image: string; title: string }[];
+  autoPlay?: boolean;
+  className?: string;
+}
+
+const List = ({ item, className, index, activeItem, ...props }: ImageProps) => {
+  return (
+    <div
+      className={cn(
+        "relative flex h-full w-20 min-w-10 cursor-pointer overflow-hidden rounded-md transition-all delay-0 duration-300 ease-in-out",
+        {
+          "flex-grow": index === activeItem,
+        },
+        className,
+      )}
+      {...props}
+    >
+      <img
+        src={item.image}
+        alt={item.title}
+        className={cn("h-full w-full object-cover", {
+          "blur-[2px]": index !== activeItem,
+        })}
+      />
+      {index === activeItem && (
+        <div className="absolute bottom-4 left-4 min-w-fit text-white md:bottom-8 md:left-8">
+          <WaveReveal
+            duration="1000ms"
+            className="items-start justify-start text-xl sm:text-2xl md:text-6xl"
+            text={item.title}
+            direction="up"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const items = [
+  {
+    image: "/images/avion-certificate.jpg",
+    title: "",
+  },
+  {
+     image: "/images/bootstrap.jpg",
+    title: "",
+  },
+  {
+    image: "/images/jquery.jpg",
+    title: "",
+  },
+];
+
+export default function Expandable({ list = items, autoPlay = true, className }: ExpandableProps) {
+  const [activeItem, setActiveItem] = useState(0);
+  const [isHovering, setIsHovering] = useState(false);
+
+  useEffect(() => {
+    if (!autoPlay) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      if (!isHovering) {
+        setActiveItem((prev) => (prev + 1) % list.length);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [autoPlay, list.length, isHovering]);
+
+  return (
+    <div className={cn("flex h-96 w-full gap-1", className)}>
+      {list.map((item, index) => (
+        <List
+          key={item.title}
+          item={item}
+          index={index}
+          activeItem={activeItem}
+          onMouseEnter={() => {
+            setActiveItem(index);
+            setIsHovering(true);
+          }}
+          onMouseLeave={() => {
+            setIsHovering(false);
+          }}
+        />
+      ))}
+    </div>
+  );
+}
