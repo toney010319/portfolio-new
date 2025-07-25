af798b432fe8db1061a9e0ce9c31ddf94c53d3e2
Anthony Bautista <anthony@mccrev.com>
2025-07-26 03:59:22 +0800

    update

--- src/component/Experience.tsx
+++ src/component/Experience.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
           Professional <span className="text-[#2EB2D3]">Experience</span>
         </h1>
         <div className="flex flex-col lg:flex-row gap-10">
-          <div data-aos="fade-up " className="text-[#EFF0F2] lg:w-1/2">
+          <div data-aos="fade-up " data-aos-delay="150" className="text-[#EFF0F2] lg:w-1/2">
             <h2 className="text-[#EFF0F2] text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">CBS OffShore</h2>
             <div className="mb-8">
               <p className="text-base md:text-lg lg:text-xl pt-3 leading-relaxed tracking-wide">
@@ -30,7 +30,7 @@ const Experience = () => {
               </p>
             </div>
           </div>
-          <div data-aos="fade-up" data-aos-delay="60" className="text-center lg:w-1/2">
+          <div data-aos="fade-up" data-aos-delay="400" className="text-center lg:w-1/2">
             <h2 className="text-[#EFF0F2] text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">Certificates</h2>
             <ImageCarousel
               items={[
