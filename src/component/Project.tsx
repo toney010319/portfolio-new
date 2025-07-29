89e4fe87038f846a486d40b7877f1c81a6f22c18
Anthony Bautista <anthony@mccrev.com>
2025-07-30 04:03:00 +0800

    update

--- src/component/Project.tsx
+++ src/component/Project.tsx
@@ -13,12 +13,20 @@ const Project = () => {
     },
 
     {
-      title: "E-Commerce Website",
+      title: "ESaverWatt",
       description:
         "My main responsibility was converting Figma prototypes into fully functional, responsive code using a mobile-first approach, ensuring compatibility across all devices. The website allows users to add items to their cart, place multiple orders with subscription options, and complete payments via PayPal and debit cards.",
       image: "/images/ecommerce.jpg",
       link: "https://shop.esaverwatt.com",
       Technologies: ["HTML", "Bootstrap", "CSS", "Jquery", ".NET"],
+    },
+    {
+      title: "ShopWise",
+      description:
+        "An ECommerce website where users can Register and Login, add to favorites the items, filter and search, add items to cart, place multiple orders with subscription options, and complete payments via PayPal and debit cards.",
+      image: "/images/wordpress.jpg",
+      link: "https://shopewise.infinityfreeapp.com/",
+      Technologies: ["Wordpress", "WooCommerce", "Elementor", "PHP", "CSS"],
     },
     {
       title: "SNAPGRAM",
