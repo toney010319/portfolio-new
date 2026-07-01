2e17a107a73f123fe3ca296added5501e0f072bd
Anthony <138202547+toney010319@users.noreply.github.com>
2026-07-02 01:44:33 +0800

    update copyright

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
       <>
       <div className='bg-gray-800 text-white text-center p-4'>
-        <p className='text-sm'>© 2025 Anthony Dev. All rights reserved.</p>
+        <p className='text-sm'>© 2026 Anthony Dev. All rights reserved.</p>
         
       </div>
       </>
